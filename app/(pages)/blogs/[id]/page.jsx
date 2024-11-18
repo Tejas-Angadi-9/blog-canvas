d614157ff6ea8dd56c23a2fa565c09e1a0945464
Tejas Angadi <tejasangadi456@gmail.com>
2024-11-18 11:28:16 +0530

    Redesigned each blog page

--- app/(pages)/blogs/[id]/page.jsx
+++ app/(pages)/blogs/[id]/page.jsx
@@ -132,137 +132,136 @@ const Blog = ({ params }) => {
   const canEditAndDelete = blogData?.userData?._id.toString() === userId;
 
   return (
-    <div className="w-11/12 flex flex-col items-start gap-2 mx-auto h-full mb-10 md:mb-20">
+    <div className="w-11/12 flex flex-col items-start gap-2 mx-auto h-full mb-10 md:mb-20 mt-5">
       {!blogData ? (
         <div className="flex w-full h-fit mx-auto items-center justify-center">
           <Loading />
         </div>
       ) : (
-        <>
-          <section className="flex flex-col p-2 md:p-5 justify-around item w-full">
-            <div className="flex w-full justify-between items-center">
-              <TagButton text={blogData?.tag} />
-              <button
-                className="flex items-center gap-2 border-2 border-black p-2 md:px-4 md:py-2 rounded-md bg-white text-black transition-all duration-200 hover:text-white hover:bg-black md:text-[16px] text-[10px]"
-                onClick={handleCopy}>
-                {copiedLink && (
-                  <>
-                    <p>Link copied Successfully</p>
-                    <div className=" rounded-full p-2font-bold">
-                      <MdDone className="text-green-500 text-[16px] md:text-[22px]" />
-                    </div>
-                  </>
-                )}
-                {!copiedLink && (
-                  <>
-                    <p>Copy link</p>
-                    <FaRegCopy className="" />
-                  </>
-                )}
-              </button>
-            </div>
-            <div>
-              <h1 className="text-[18px] md:text-[45px] md:w-[65%] font-bold mt-5">
-                {/* The Impact of Technology on the Workplace: How Technology is
-            Changing */}
-                {blogData?.title}
-              </h1>
-            </div>
-            <div className="flex gap-2 items-center mt-5 justify-between w-[62%]">
-              <div className="flex flex-row items-center justify-center gap-2">
-                <div className="flex items-center justify-center gap-2 xl:gap-5 text-slate-500">
-                  {/* This contains the image of the user */}
-                  {userData?.profileImage ? (
-                    <img
-                      src={`${userData?.profileImage}`}
-                      alt=""
-                      width={40}
-                      height={40}
-                      className="w-6 h-6 xl:w-10 xl:h-10 rounded-full"
-                      loading="lazy"
-                    />
+        <div className="flex flex-col md:flex-row">
+          {/* This section contains the image and the blog description */}
+
+          <section className="md:flex flex-col items-start justify-start md:pr-10 w-full md:border-r-2 rounded-s-lg gap-4 md:gap-5">
+            <section className="flex flex-col px-6 md:p-5 justify-center item w-full">
+              <div className="flex w-full justify-between items-center">
+                <TagButton text={blogData?.tag} />
+                <button
+                  className="flex items-center gap-2 border-2 border-black p-2 md:px-4 md:py-2 rounded-md bg-white text-black transition-all duration-200 hover:text-white hover:bg-black md:text-[16px] text-[10px]"
+                  onClick={handleCopy}>
+                  {copiedLink && (
+                    <>
+                      <p>Link copied Successfully</p>
+                      <div className=" rounded-full p-2font-bold">
+                        <MdDone className="text-green-500 text-[16px] md:text-[22px]" />
+                      </div>
+                    </>
+                  )}
+                  {!copiedLink && (
+                    <>
+                      <p>Copy link</p>
+                      <FaRegCopy className="" />
+                    </>
+                  )}
+                </button>
+              </div>
+              <div>
+                <h1 className="text-[18px] md:text-[45px] w-full font-bold mt-5">
+                  {/* The Impact of Technology on the Workplace: How Technology is Changing */}
+                  {blogData?.title}
+                </h1>
+              </div>
+              <div className="flex gap-2 items-center mt-5 justify-between w-[95%] md:w-full">
+                <div className="flex flex-row items-center justify-center gap-2">
+                  <div className="flex items-center justify-center gap-2 xl:gap-5 text-slate-500">
+                    {/* This contains the image of the user */}
+                    {userData?.profileImage ? (
+                      <img
+                        src={`${userData?.profileImage}`}
+                        alt=""
+                        width={40}
+                        height={40}
+                        className="w-6 h-6 xl:w-10 xl:h-10 rounded-full"
+                        loading="lazy"
+                      />
+                    ) : (
+                      <img
+                        src={`https://api.dicebear.com/9.x/initials/svg?seed=${userData?.name}`}
+                        alt=""
+                        width={40}
+                        height={40}
+                        className="w-6 h-6 xl:w-10 xl:h-10 rounded-full"
+                        loading="lazy"
+                      />
+                    )}
+                  </div>
+                  {/* Author name */}
+                  <div className="flex flex-row justify-between gap-5">
+                    <p className="text-[11px] xl:text-lg font-semibold">
+                      {/* Jason Francisco */}
+                      {userData?.name}
+                    </p>
+                    {/* Published date */}
+                    <p className="text-slate-500 text-[11px] xl:text-lg">
+                      {/* August 20, 2022 */}
+                      {formattedDate}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex gap-1 items-center justify-center pb-1 pt-2">
+                  {hasUserLiked ? (
+                    <button
+                      className="flex md:flex gap-1 items-center justify-center text-[14px]  md:text-[22px]"
+                      onClick={unlikeHandler}>
+                      {loading ? (
+                        // <Loading />
+                        <Spinner />
+                      ) : (
+                        <>
+                          <FaHeart className="text-red-500 mr-1 text-[18px] md:text-[22px]" />
+                          <p className="">
+                            {likedUsers?.length > 0 ? likedUsers?.length : 0}
+                          </p>
+                        </>
+                      )}
+                    </button>
                   ) : (
-                    <img
-                      src={`https://api.dicebear.com/9.x/initials/svg?seed=${userData?.name}`}
-                      alt=""
-                      width={40}
-                      height={40}
-                      className="w-6 h-6 xl:w-10 xl:h-10 rounded-full"
-                      loading="lazy"
-                    />
+                    <button
+                      className="flex md:flex gap-1 items-center justify-center text-[14px]  md:text-[22px]"
+                      onClick={likeHandler}>
+                      {loading ? (
+                        // <Loading />
+                        <Spinner />
+                      ) : (
+                        <>
+                          <CiHeart className="mr-0 text-[22px] md:text-[22px]" />
+                          <p className="">
+                            {likedUsers?.length > 0 ? likedUsers?.length : 0}
+                          </p>
+                        </>
+                      )}
+                    </button>
                   )}
                 </div>
-                {/* Author name */}
-                <div className="flex flex-row justify-between gap-5">
-                  <p className="text-[11px] xl:text-lg font-semibold">
-                    {/* Jason Francisco */}
-                    {userData?.name}
-                  </p>
-                  {/* Published date */}
-                  <p className="text-slate-500 text-[11px] xl:text-lg">
-                    {/* August 20, 2022 */}
-                    {formattedDate}
-                  </p>
-                </div>
               </div>
-              <div className="flex gap-1 items-center justify-center pb-1 pt-2">
-                {hasUserLiked ? (
-                  <button
-                    className="flex md:flex gap-1 items-center justify-center text-[14px]  md:text-[22px]"
-                    onClick={unlikeHandler}>
-                    {loading ? (
-                      // <Loading />
-                      <Spinner />
-                    ) : (
-                      <>
-                        <FaHeart className="text-red-500 mr-1" />
-                        <p className="">
-                          {likedUsers?.length > 0 ? likedUsers?.length : 0}
-                        </p>
-                      </>
-                    )}
-                  </button>
-                ) : (
-                  <button
-                    className="flex md:flex gap-1 items-center justify-center text-[14px]  md:text-[22px]"
-                    onClick={likeHandler}>
-                    {loading ? (
-                      // <Loading />
-                      <Spinner />
-                    ) : (
-                      <>
-                        <CiHeart className="text-[28px]" />
-                        <p className="">
-                          {likedUsers?.length > 0 ? likedUsers?.length : 0}
-                        </p>
-                      </>
-                    )}
-                  </button>
-                )}
-              </div>
-            </div>
-          </section>
-
-          {/* This section contains the image and the blog description */}
-          <section className="md:flex md:flex-row flex-col items-start justify-center md:px-5 w-full h-full gap-4 md:gap-10">
-            <div className="flex flex-col flex-1 items-center justify-center md:items-start md:justify-start">
-              <div className="relative w-[80%] md:w-[90%] h-[300px] flex md:h-[500px]">
-                {/* This contains the blog image */}
+            </section>
+            <div className="flex flex-col items-center justify-center mx-auto mt-3 md:mt-0 pr-4 md:pr-0 md:w-full">
+              <div className="relative w-[90%] md:w-[800px] md:h-[500px]">
+                {/* Ensure images fit the container with proper scaling */}
                 <Image
-                  // src="https://images.pexels.com/photos/3184454/pexels-photo-3184454.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
                   src={blogData?.blogImage}
                   alt="blogImage"
-                  layout="fill"
-                  className="object-cover rounded-md opacity-95"
+                  width={800}
+                  height={500} // Adjust as needed
+                  className="object-contain rounded-md"
                   loading="lazy"
                 />
               </div>
-              <div className="pt-6 md:pt-10 bg-white bg-opacity-90 backdrop-blur-md w-[95%] md:w-[90%] flex text-justify">
+              <div className="pt-6 md:pt-10 bg-white bg-opacity-90 backdrop-blur-md w-[90%] md:w-[100%] flex text-justify">
                 <p className="text-[#3B3C4A] text-[12px] md:text-[18px] leading-relaxed">
                   {blogData?.description}
                 </p>
               </div>
-              {canEditAndDelete == true && (
+              {canEditAndDelete && (
                 <div className="w-[90%] flex items-center gap-5 md:pt-10 h-full mt-5">
                   <button className="flex border-2 border-black items-center justify-center text-[12px] md:text-[14px] p-1 w-fit md:px-4 md:py-2 rounded-md outline-none">
                     Edit
@@ -273,11 +272,11 @@ const Blog = ({ params }) => {
                 </div>
               )}
             </div>
-            <aside className="flex flex-col mt-5 md:-mt-20">
-              <MoreBlogs text="More Blogs" />
-            </aside>
           </section>
-        </>
+          <aside className="flex flex-col mt-5">
+            <MoreBlogs text="More Blogs" />
+          </aside>
+        </div>
       )}
     </div>
   );
--- components/blogsSection/MoreBlogs.jsx
+++ components/blogsSection/MoreBlogs.jsx
@@ -26,14 +26,14 @@ const MoreBlogs = ({ text }) => {
   return (
     <>
       {/* Heading */}
-      <div className="font-bold text-[28px] px-6 xl:block flex items-center w-full justify-center xl:text-[32px]">
+      <div className="font-bold text-[24px] px-6 xl:block flex items-center w-full justify-center xl:text-[32px]">
         {text}
       </div>
 
       {/* All Blogs Cards */}
       {/* Blog 1 card */}
       <div
-        className={`w-full h-fit xl:p-6 rounded-xl gap-5 items-center flex flex-col overflow-x-hidden ${
+        className={`w-[97%] h-fit xl:p-6 rounded-xl gap-5 items-center flex flex-col overflow-x-hidden ${
           blogs && "flex flex-col"
         }  ${!blogs && "flex items-center justify-center"}`}>
         {!blogs ? (
@@ -53,11 +53,11 @@ const MoreBlogs = ({ text }) => {
               <Link
                 key={index}
                 href={`/blogs/${blog?._id}`}
-                className="flex md:w-[450px] w-[300px] h-full gap-2">
+                className="flex md:w-[450px] w-[400px] h-full gap-2">
                 <div className="flex w-full h-fit gap-5" key={blog._id}>
                   <div className="h-fit items-center border-b-2 border-b-slate-200 justify-between w-full flex gap-10 p-4 cursor-pointer py-2 pb-6">
                     {/* Image */}
-                    <div className="flex w-[50%] h-[150px] relative">
+                    <div className="flex w-[50%] h-[120px] relative">
                       <Image
                         src={`${blog?.blogImage}`}
                         alt="blog1.png"
@@ -67,15 +67,15 @@ const MoreBlogs = ({ text }) => {
                       />
                     </div>
 
-                    <div className="w-[50%] flex flex-col items-start justify-start">
+                    <div className="w-[50%] flex flex-col items-start justify-start mt-5 md:mt-0">
                       {/* Tag and paragraph */}
-                      <div className="flex flex-col w-[120%]">
+                      <div className="flex flex-col md:w-[120%]">
                         {/* Author image, title and date */}
-                        <h3 className="font-semibold text-[16px]">
+                        <h3 className="font-semibold text-[13px] md:text-[16px]">
                           {blog?.title}
                           {/* The Impact of Technology on the Workplace: How Technology is Changing */}
                         </h3>
-                        <p className="text-blue-500 font-normal text-[12px] xl:text-[12px] mt-2">
+                        <p className="text-blue-500 font-normal text-[11px] xl:text-[12px] mt-2">
                           {blog?.tag}
                         </p>
                       </div>
@@ -87,7 +87,7 @@ const MoreBlogs = ({ text }) => {
                               alt=""
                               width={20}
                               height={20}
-                              className="w-3 h-3 xl:w-8 xl:h-8 rounded-full"
+                              className="w-6 h-6 xl:w-8 xl:h-8 rounded-full"
                               loading="lazy"
                             />
                           ) : (
@@ -96,18 +96,18 @@ const MoreBlogs = ({ text }) => {
                               alt={`${userData?.name}.svg`}
                               width={20}
                               height={20}
-                              className="w-3 h-3 xl:w-8 xl:h-8 rounded-full"
+                              className="w-6 h-6 xl:w-8 xl:h-8 rounded-full"
                               loading="lazy"
                             />
                           )}
                           <div className="flex flex-col items-start justify-center">
                             {/* Author name */}
-                            <p className="text-[12px] xl:text-[14px]">
+                            <p className="text-[11px] xl:text-[14px]">
                               {/* Jason Francisco */}
                               {userData?.name}
                             </p>
                             {/* Published date */}
-                            <p className="text-slate-500 text-[12px] xl:text-[14px]">
+                            <p className="text-slate-500 text-[11px] xl:text-[14px]">
                               {/* August 20, 2022 */}
                               {formattedDate}
                             </p>
