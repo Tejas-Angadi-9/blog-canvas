5676be9504c67bb3e3392622590e5871fc6315c4
Tejas Angadi <tejasangadi456@gmail.com>
2024-12-01 15:44:09 +0530

    Removed the local file storage before sending it to cloudinary

--- app/api/user/updateUser/route.js
+++ app/api/user/updateUser/route.js
@@ -3,8 +3,6 @@ import User from "@/models/User";
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 import cloudinaryUploader from "@/services/cloudinaryUploader";
-import path from "path"
-import { promises as fs } from "fs";
 
 export const PATCH = async (req) => {
     const cookieStore = cookies();
@@ -30,9 +28,7 @@ export const PATCH = async (req) => {
                 message: 'Token has expired. Please login again'
             }), { status: 404 })
         }
-        // Storing the userId from the payload data
         const userId = decoded.userId;
-        // Validating whether the user is signed up or not
         const existingUser = await User.findById(userId).populate("createdBlogs").exec();
         if (!existingUser) {
             return new Response(JSON.stringify({
@@ -47,22 +43,19 @@ export const PATCH = async (req) => {
 
         if (profileImage) {
             const buffer = Buffer.from(await profileImage.arrayBuffer());
-            const uploadPath = path.join(process.cwd(), 'image-uploads', profileImage.name);
-            await fs.writeFile(uploadPath, buffer);
-
-            const imageUploadResult = await cloudinaryUploader(uploadPath, "Blog-Canvas");
+            // Directly upload the image to Cloudinary
+            const imageUploadResult = await cloudinaryUploader(buffer, "Blog-Canvas");
 
             await existingUser.updateOne(
                 {
                     profileImage: imageUploadResult.secure_url,
                 }
             );
-            await fs.unlink(uploadPath, buffer);
         }
 
         const updatedUser = await User.findById(userId).populate("createdBlogs").exec();
-
         const { password: _, ...profileDetails } = updatedUser.toObject();
+
         return new Response(JSON.stringify({
             status: true,
             message: "Successfully updated the profile image",
@@ -70,11 +63,11 @@ export const PATCH = async (req) => {
         }), { status: 201 })
     }
     catch (err) {
-        console.log('Internal server while updating the profile image')
+        console.log('Internal server error while updating the profile image');
         return new Response(JSON.stringify({
             status: false,
             message: err.message,
-            data: "Internal server while updating the profile image"
+            data: "Internal server error while updating the profile image"
         }), { status: 500 })
     }
 }
