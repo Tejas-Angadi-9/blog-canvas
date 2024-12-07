894b00ac74c3795d544b96a2852bc58063f2cec8
Tejas Angadi <tejasangadi456@gmail.com>
2024-12-07 17:45:27 +0530

    Reverted the forgotpasswordFields to screen height.

--- components/forgotPasswordSection/VerifyingLink.jsx
+++ components/forgotPasswordSection/VerifyingLink.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const VerifyingLink = () => {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-screen">
       <h2 className="text-[24px] md:text-[30px] font-medium -translate-y-[50%]">Verifying link...</h2>
     </div>
   );
